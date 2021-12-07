d37fb605d9be173f21ec659a0aa1f668968186ef
Karson Durocher <karson@durochers.org>
2021-12-06 22:38:38 -0500

    update 1.1

--- spotigram.js
+++ spotigram.js
@@ -1,33 +1,42 @@
+/*
+    Author:
+    @kars0nn
+
+    !!NOTICE!! Please make sure you have your own spotify app IDs if you are using this in development mode
+*/
 const path = require('path');
 let config;
 const SpotifyWebApi = require('spotify-web-api-node');
 const { IgApiClient } = require('instagram-private-api')
 const deployPath = path.dirname(process.execPath);
+const fs = require('fs')
 const express = require('express');
 const app = express();
 const ig = new IgApiClient()
 const prompt = require('prompt');
-let version = 'beta 0.1'
-
+const colors = require('colors');
+const open = require('open')
 config = JSON.parse(require('fs').readFileSync(path.join(deployPath, 'config.json'), 'utf8'));
 //config = require('./config.json')
 
+//Define lastsong, accesstoken, and refreshtoken for later use
 let lastSong;
-let accessToken = config.accessToken;
-let refreshToken = config.refreshToken;
-
-let spotifyApi;
-
-tryLogin()
+let accessToken;
+let refreshToken;
+const spotifyApi = new SpotifyWebApi({
+    clientId: '',//you need to set this
+    clientSecret:'', //you need to set this
+    redirectUri: 'http://localhost:8000/callback'
+});
+let version = '1.1'
 
 async function tryLogin(u, p){
-    if(u != undefined || p != undefined) {
-        return login(u, p);
-    } else {
+    let r = await checkForUpdates()
+    if(!r){
         prompt.start();
-        prompt.message = "Spotigram"
+        prompt.message = "Spotigram".green
         prompt.delimiter = ": "
-        const {username, email, password} = await prompt.get([{
+        const {username, password} = await prompt.get([{
             name:'username',
             description:'Instagram Username',
             required: true
@@ -38,48 +47,77 @@ async function tryLogin(u, p){
             hidden: true,
             replace: '*'
         }]);
-        return login(username, email, password);
+        return login(username, password);
+    } else {
+        setTimeout(() => {
+            process.exit()
+        },2000)
     }
 }
 
-async function login(u, e, p) {
-    console.log('Logging in to Instagram...')
+const tipArray = [ //random tips!
+    "spotigram!",
+    "You can edit what Spotigram sets as your bio in config.json!",
+    'To have a more personalized bio, you can write \\n in between your personal stuff, and spotify activity! Ex. "I love pizza! \\n Listening to -"',
+    "birds don't sing!",
+    "no pain, no gain!",
+    "thank you for using my app! it means a lot, i'll be sure to keep it updated and easy for everyone <3 ~ karson",
+    "2fa integration is hard lol",
+    "karson's favorite song is Walking On A Dream by Empire of The Sun!",
+    "i wrote this program on 300mg of caffine one night LMAO",
+    "hfdjsafbvpvayvehfvlbldahcbvlawelhfLVA ~ karson <3 (this isn't a secret code, I just slammed my keyboard",
+    "WARNING! Errors may occur! (will occur*)",
+    "who's your favorite artist?",
+    "what's one song you would play for the world if you had the chance?"
+]
+
+async function login(u, p) {
+    const random = Math.floor(Math.random() * tipArray.length);
+    console.log('------------------------'.bold.cyan)
+    console.log('Logging in to Instagram...'.italic.grey)
     ig.state.generateDevice(u)
     await ig.simulate.preLoginFlow();
     try {
         loggedInUser = await ig.account.login(u, p);
     } catch (err) {
-        console.log('Something went wrong while logging in (incorrect username or password?), Try again! ')
-        console.log('- If your account has 2fa/mfa, support for this will come in the next update. Stay tuned!')
+        console.log('Something went wrong while logging in (incorrect username or password?), Try again!'.red)
+        console.log('- If your account has 2fa/mfa, support for this will come in futre updates. Stay tuned!'.yellow)
         return tryLogin();
     }
     process.nextTick(async () => await ig.simulate.postLoginFlow());
-    console.log("Logged in successful! Make sure to trust the new device on your actual instagram app.")
-    console.log('------------------------')
+    console.log("Logged in successfully! Make sure to trust the new device on your actual instagram app if you haven't already.".italic.grey)
+    console.log('------------------------'.bold.cyan)
+    console.log(`TIP: ${tipArray[random]}`.italic.magenta)
+    console.log('------------------------'.bold.cyan)
+    console.log('Seeing if you have an access token already...'.italic.grey)
     await startServer()
-    if(!config.spotifyClientID || !config.spotifyClientSecret){
-        return console.log(`Please visit http://localhost:8000/ in your browser to set up your spotify client and link your account. Restart this app when you're done with every step.`)
-    } else {
-        spotifyApi = new SpotifyWebApi({
-            clientId: config.spotifyClientID,
-            clientSecret: config.spotifyClientSecret,
-            redirectUri: 'http://localhost:8000/callback'
-        });
-    }
     setTimeout(() => {
-        startApp()
+        if(!config.accessToken || !config.refreshToken) {
+            return linkSpotify();
+        } else {
+            return startApp();
+        }
     }, 2000)
 }
 
-async function startApp(){
-    spotifyApi.setAccessToken(accessToken)
-    spotifyApi.setRefreshToken(refreshToken)
+async function linkSpotify() { //this initiates when a user does not have access tokens in config.json
+    console.log("opening Spotify authorization link, hang tight...")
+    setTimeout(() => {
+        open('https://accounts.spotify.com/authorize?client_id=9459e0d038db4b2a98554eb5b085adaf&response_type=code&redirect_uri=http://localhost:8000/callback&scope=user-read-playback-state%20user-read-currently-playing%20user-top-read%20user-read-recently-played')
+    }, 1000)
+}
+
+async function startApp(){ //this initiates if the use has an access token
+    console.log('You do! Starting Spotigram up right now!'.italic.grey)
+    console.log('------------------------'.bold.cyan)
+    spotifyApi.setAccessToken(config.accessToken)
+    spotifyApi.setRefreshToken(config.refreshToken)
     get();
 }
 
-async function get() {
-    if(!accessToken || !refreshToken) {
-        return console.log('You need to link your Spotify account, please go to http://localhost:8000/link in your browser to link your account and get your full config.json file to copy and paste')
+async function get() { //main bio update function
+    if(!config.accessToken || !config.refreshToken) {
+        return console.log('You have not linked your spotify yet. Do it manually by going to http://localhost:8000/link in your browser.'.yellow)
     } else {
         setInterval(() => {
             spotifyApi.getMyCurrentPlayingTrack().then(function (data) {
@@ -89,10 +127,10 @@ async function get() {
                             return;
                         } else if(lastSong != data.body.items[0].track.id){
                             lastSong = data.body.items[0].track.id
-                            console.log(`${data.body.items[0].track.name} by ${data.body.items[0].track.artists[0].name} was the last song you listened to.`)
+                            console.log(`${data.body.items[0].track.name} by ${data.body.items[0].track.artists[0].name} was the last song you listened to.`.magenta)
                             return ig.account.setBiography(`${config.recentBioText} ${data.body.items[0].track.name} by ${data.body.items[0].track.artists[0].name}`)
                         } else {
-                            return console.log('error? lmao')
+                            return console.log('error? lmao'.red)
                         }
                     })
                 } else {
@@ -100,10 +138,10 @@ async function get() {
                         return;
                     } else if(lastSong !== data.body.item.id){
                         lastSong = data.body.item.id
-                        console.log(`Currently Listening to ${data.body.item.name} by ${data.body.item.artists[0].name}`)
+                        console.log(`Currently Listening to ${data.body.item.name} by ${data.body.item.artists[0].name}`.magenta)
                         return ig.account.setBiography(`${config.bioText} ${data.body.item.name} by ${data.body.item.artists[0].name}`)
                     } else {
-                        return console.log('error? lmao')
+                        return console.log('error? lmao'.red)
                     }
                 }
             }, function (err) {
@@ -118,10 +156,10 @@ async function get() {
                                     return;
                                 } else if(lastSong != data.body.items[0].track.id){
                                     lastSong = data.body.items[0].track.id
-                                    console.log(`${data.body.items[0].track.name} by ${data.body.items[0].track.artists[0].name} was the last song you listened to.`)
+                                    console.log(`${data.body.items[0].track.name} by ${data.body.items[0].track.artists[0].name} was the last song you listened to.`.magenta)
                                     return ig.account.setBiography(`${config.recentBioText} ${data.body.items[0].track.name} by ${data.body.items[0].track.artists[0].name}`)
                                 } else {
-                                    return console.log('error? lmao')
+                                    return console.log('error? lmao'.red)
                                 }
                             })
                         } else {
@@ -129,14 +167,14 @@ async function get() {
                                 return;
                             } else if(lastSong !== data.body.item.id){
                                 lastSong = data.body.item.id
-                                console.log(`Currently Listening to ${data.body.item.name} by ${data.body.item.artists[0].name}`)
+                                console.log(`Currently Listening to ${data.body.item.name} by ${data.body.item.artists[0].name}`.magenta)
                                 return ig.account.setBiography(`${config.bioText} ${data.body.item.name} by ${data.body.item.artists[0].name}`)
                             } else {
-                                return console.log('error? lmao')
+                                return console.log('error? lmao'.red)
                             }
                         }
                     }, function (err) {
-                        console.log('error: ' + err)
+                        console.log(`error: ${error}`.red)
                     })
                 })
             })
@@ -144,28 +182,11 @@ async function get() {
     }
 }
 
-async function startServer(){
-    app.get('/', (req, res) => {
-        res.send(`<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Spotigram App Making</title> </head> <body> <body style="background-color: #303030;"> <br> <div style="text-align: center; color: rgb(0, 195, 255); font-weight: 500; font-family: Arial, Helvetica, sans-serif;"> <h1>Welcome to Spotigram.</h1> <br> <a>To get started, you need to have these things:</a> <br> <br> <li>An Instagram account</li> <li>A Spotify account</li> <li>A spotify developer app (<a href="https://developer.spotify.com/dashboard/" style="color: rgb(0, 255, 34);">developers.spotify.com</a>) - These are free to make, and easy. There is a tutorial below</li> <li>Little knowledge of technology</li> </div> <br> <br> <div style="text-align: center; color: rgb(255, 145, 0); font-weight: 500; font-family: Arial, Helvetica, sans-serif;"> <h2>For more help with the Spotify app, follow these instructions:</h2> <p> 1.) Log in to <a href="https://developer.spotify.com/dashboard/" style="color: rgb(0, 255, 34);">https://developer.spotify.com/dashboard/</a> if not already<br><br> 2.) Click on the 'Create An App' button<br><br> 3.) Name the app whatever you want, make sure to give it a description, agree, then click the 'Create' button<br><br> 4.) After you click create, you will be greeted with a dashboard, it's nothing much, don't worry.<br><br> 5.) Click the 'Edit Settings' button<br><br> 6.) Under the 'Redirect URIs' section, type this: http://localhost:8000/callback/<br><br> 7.) Once you're done adding the url above, click 'Add'<br><br> 8.) Scroll down and click the 'Save' button<br><br><br> You're almost there! A few more steps to go...<br><br><br> 9.) Find where the page says 'Client ID' and copy the line of random letters and numbers next to it. Ex: f2284a887fa9487495ac54213404fff0<br><br> 10.) Go to your config.json file in the 'Spotigram' folder, and paste the Client ID in the quotes next to "spotifyClientID": then save the file<br> EXAMPLE - "spotifyClientID":"" --> "spotifyClientID":"f2284a887fa9487495ac54213404fff0"<br><br> 11.) You'll see that spotifyClientSecret is not defined, so go back to the spotify app dashboard in your browser<br><br> 12.) Click the 'Show Client Secret' text, then copy the long random string next to 'Client Secret'<br><br> 13.) Go back to the config.json file in the 'Spotigram' folder, then paste your client secret in the quotes next to "spotifyClientSecret": then save the file<br> EXAMPLE - "spotifyClientSecret":"" --> "spotifyClientSecret":"dhd721s8337gs7gag7ifdhH871173Gkjd0"<br><br> 14.) Double check you followed each step carefully...<br><br> 15.) You did it! The last thing you need to do will be to visit <a href="/link" style="color: rgb(0, 255, 34);">localhost:8000/link</a><br>Make sure to put the accessToken and refreshToken the correct spots in the config, or else it won't work.</p> </div> <br> <br> <br> </body> </body> </html>`)
+async function startServer(){ //web server for direct links and callback for spotify auth (callback needs to stay callback)
+    app.get('/link', (req, res) => {
+        res.redirect('https://accounts.spotify.com/authorize?client_id=9459e0d038db4b2a98554eb5b085adaf&response_type=code&redirect_uri=http://localhost:8000/callback&scope=user-read-playback-state%20user-read-currently-playing%20user-top-read%20user-read-recently-played')
     })
-    app.get('/link', async (req, res) => {
-        if(!config.spotifyClientID || !config.spotifyClientSecret) {
-            return res.send('Please restart the app if you have added the clientID and secret to the config. If you have not done this yet, please follow the steps on http://localhost:8000/')
-        }
-        const scopes = [
-            'user-read-playback-state',
-            'user-read-currently-playing',
-            'user-read-private',
-            'playlist-read-collaborative',
-            'playlist-read-private',
-            'user-library-read',
-            'user-top-read',
-            'user-read-recently-played',
-            'user-follow-read'
-        ];
 
-        res.redirect(spotifyApi.createAuthorizeURL(scopes))
-    })
     app.get('/callback', async (req, res) => {
         const code = req.query.code;
         spotifyApi.authorizationCodeGrant(code)
@@ -173,14 +194,23 @@ async function startServer(){
                 const access_token = data.body['access_token'];
                 const refresh_token = data.body['refresh_token'];
 
-                accessToken = access_token
+                accessToken = access_token //set local access token vars so we don't have to restart
                 refreshToken = refresh_token
 
-                console.log('Access and Refresh token obtained successfully.')
-                return res.send(`<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Spotigram App Making</title> </head> <body> <body style="background-color: #303030;"> <br> <div style="text-align: center; color: rgb(0, 195, 255); font-weight: 500; font-family: Arial, Helvetica, sans-serif;"> <h1>Please copy and paste all text in green below and paste into your config.json file and restart the app.</h1><br><a>the text should replace everything you have in the file</a><br> </div> <div> <code style="color: rgb(102, 255, 122);"> {<br> "spotifyClientID":"${config.spotifyClientID}",<br> "spotifyClientSecret":"${config.spotifyClientSecret}",<br> "accessToken":"${access_token}",<br> "refreshToken":"${refresh_token}",<br><br> "bioText":"🎶 Listening to -",<br> "recentBioText":"Recently listened to -"<br> }<br> </code> </div> <br> </body> </body> </html>`)
+                config.accessToken = access_token // change values in config and then ->
+                config.refreshToken = refresh_token
+
+                fs.writeFile(path.join(deployPath, 'config.json'), JSON.stringify(config, null, 2), (err) => { // write newly defined tokens to config.json
+                    if (err) throw err;
+                });
+
+                console.log('Access and Refresh token obtained successfully.'.green)
+                startApp()
+                return res.send(`<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Spotigram</title> </head> <body style="background-color: rgb(29, 29, 29); color: white; font-family: Arial, Helvetica, sans-serif;"> <br> <br> <br> <div style="text-align: center;"> <h1>Spotify account linked successfully!</h1><p><i>you can go back to whatever you were doing now!</i></p> <br> <br> <br> <p>Thank you for using my app, Enjoy!</p> <i> <a href="https://github.com/kars0nn" style="text-decoration: none; color: rgb(0, 183, 255);"> ~ karson </a> <a style="color: rgb(255, 0, 170);">:3</a> </i> </div> </body> </html>`)
             })
             .catch(error => {
                 res.send('Something went wrong while authorizing your code!')
+                console.log(error)
             })
     })
 
@@ -188,5 +218,33 @@ async function startServer(){
         res.send('<h1>404</h1><br><a>Page not found</a>')
     })
 
-    app.listen(8000, () => { console.log(`Starting Spotigram ${version}`)});
+    app.listen(8000);
 }
+
+async function checkForUpdates(){ //checks for updates and lets user know if they are up to date or not
+    const p = require('phin')
+
+    const res = await p('https://wsdpanthers.net/spoti_ver')//update page
+    let j = JSON.parse(res.body)
+    if(j.version === version){
+        console.log('Spotigram is up to date! {v1.1}'.italic.grey)
+    } else {
+        console.log('There is a new version of spotigram available, do you wish to update?'.italic.red)
+        prompt.start();
+        prompt.message = ""
+        prompt.delimiter = ""
+        const {quest} = await prompt.get([{
+            name:'quest',
+            description:'yes / no'
+        }]);
+        if(quest === 'yes') {
+            open('https://github.com/kars0nn/Spotigram/releases')
+            return true;
+        } else {
+            console.log('OK')
+            return false;
+        }
+    }
+}
+
+tryLogin()
